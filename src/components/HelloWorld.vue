33bdd0170c334fb355b0dfe48281a44254d9b376
DerModMaster <dermodmaster@googlemail.com>
2020-11-10 14:49:31 +0100

    init repo

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,223 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <v-content class="pt-0">
+        <v-row>
+            <v-col v-if="showMap" cols="7">
+                <l-map
+                        :zoom="zoom"
+                        :center="center"
+                        :style="'height: '+this.$vuetify.breakpoint.height+'px; width: 100%'"
+                >
+                    <l-tile-layer
+                            :url="url"
+                            :attribution="attribution"
+                    />
+                    <l-control-fullscreen position="topleft"
+                                          :options="{ title: { 'false': 'Fullscreen', 'true': 'Exit' } }"
+                    />
+                    <div v-for="(repeater, repeaterIndex) in repeaters" v-bind:key="repeaterIndex">
+                        <div v-for="(rx,rx_key) in repeater.rx" v-bind:key="rx_key">
+                            <l-marker
+                                    :lat-lng="[getAntennaForReceiver(repeater, rx_key).location.lat, getAntennaForReceiver(repeater, rx_key).location.lon]"
+                                    :icon="icon">
+                                <l-tooltip>{{repeater.callsign+" - "+rx.name}}</l-tooltip>
+                            </l-marker>
+                            <l-marker
+                                    :lat-lng="[getAntennaForReceiver(repeater, rx_key).location.lat, getAntennaForReceiver(repeater, rx_key).location.lon]"
+                                    v-if="rx.info.sql === 'active'" :icon="receivingicon">
+                                <l-tooltip>{{repeater.callsign+" - "+rx.name}}</l-tooltip>
+                            </l-marker>
+                            <l-circle-marker
+                                    :lat-lng="[getAntennaForReceiver(repeater, rx_key).location.lat, getAntennaForReceiver(repeater, rx_key).location.lon]"
+                                    :radius="rx.info.lvl*1.5"
+                                    v-if="rx.info.sql === 'active'"
+                                    fill-color="green"
+                                    weight="1"
+                                    color="green"
+                            />
+                            <l-circle-marker
+                                    :lat-lng="[getAntennaForReceiver(repeater, rx_key).location.lat, getAntennaForReceiver(repeater, rx_key).location.lon]"
+                                    :radius="150"
+                                    v-if="rx.info.sql === 'active'"
+                                    fill-opacity="0"
+                                    weight="3"
+                                    color="grey"
+                            />
+                        </div>
+                        <div v-for="(tx,tx_key) in repeater.txlist" v-bind:key="tx_key">
+                            <l-marker
+                                    :lat-lng="[getAntennaForReceiver(repeater, tx).location.lat, getAntennaForReceiver(repeater, tx).location.lon]"
+                                    v-if="repeater.tx === '1'" :icon="sendingicon">
+                                <l-tooltip>{{repeater.callsign+" - "+tx_key}}</l-tooltip>
+                            </l-marker>
+                        </div>
+                    </div>
+                </l-map>
+            </v-col>
+            <v-col>
+                <v-container>
+                    <v-row>
+                        <v-col v-for="(repeater, repeaterIndex) in repeaters" v-bind:key="repeaterIndex" cols="4">
+                            <v-card :color="repeater.tx === '1' ? 'green darken-4' : ''">
+                                <v-card-title>
+                                    <img v-if="repeater.tx === '1'" src="@/assets/icons/sending-green120.gif"
+                                         style="max-height: 120px">
+                                    <img v-else src="@/assets/icons/station-off.png"
+                                         style="max-height: 120px; filter: invert()">
+                                    {{repeater.callsign}}
+                                </v-card-title>
+                                <v-card-text>
+                                    <div v-for="(rx, rx_key) in repeater.rx" v-bind:key="rx_key">
+                                        <center>{{rx.name}}</center>
+                                        <v-progress-linear :value="rx.info.lvl"
+                                                           :color="rx.info.sql === 'closed' ? 'red' : 'green'"
+                                                           height="20">{{rx.info.lvl}}
+                                        </v-progress-linear>
+
+                                    </div>
+                                </v-card-text>
+                            </v-card>
+                        </v-col>
+                    </v-row>
+                </v-container>
+            </v-col>
+        </v-row>
+    </v-content>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+    import {latLng, icon} from "leaflet";
+    import axios from 'axios';
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+    export default {
+        name: 'Map',
+        created() {
+            window.addEventListener("resize", () => {
+                this.showMap = false;
+                setTimeout(() => {
+                    this.showMap = true;
+                },300)
+
+            })
+        },
+        mounted() {
+            axios.get("https://signal.3ef.de/api/repeaters.json").then(r => {
+                const data = r.data["info"];
+
+                for (let repeaterAttribute in r.data["info"]) {
+                    const jsonUrl = data[repeaterAttribute].uri.replace("http://do7sk1.ddns.net/", "https://signal.3ef.de/api/");
+                    axios.get(jsonUrl).then(r => {
+                        let repeater = r.data;
+                        this.$sse(repeater.uri.signalEventStream, {format: 'json'})
+                            .then(sse => {
+                                repeater.sse = sse;
+                                repeater.messages = [];
+                                repeater.tx = "0";
+                                for (let rx in repeater.rx) {
+                                    repeater.rx[rx].info = {"lvl": -1, "sql": "closed"};
+                                }
+
+                                sse.onError(e => {
+                                    console.error('lost connection; giving up!', e);
+
+                                    // This is purely for example; EventSource will automatically
+                                    // attempt to reconnect indefinitely, with no action needed
+                                    // on your part to resubscribe to events once (if) reconnected
+                                    sse.close();
+                                });
+
+                                // Listen for messages without a specified event
+                                sse.subscribe('', (message) => {
+                                    if (message.event === "Logic:transmit") {
+                                        repeater.tx = message.tx;
+                                    } else if (message.event === "Voter:sql_state") {
+                                        for (let att in message.rx) {
+                                            repeater.rx[att].info = message.rx[att];
+                                        }
+                                    }
+                                });
+
+                                this.repeaters.push(repeater);
+                                console.log("Loaded ", repeater);
+                            })
+                            .catch(err => {
+                                // When this error is caught, it means the initial connection to the
+                                // events server failed.  No automatic attempts to reconnect will be made.
+                                console.error('Failed to connect to server', err);
+                            });
+
+
+                    })
+                }
+            })
+        },
+
+        data: () => ({
+            repeaters: [],
+            showMap: true,
+            zoom: 8,
+            center: latLng(52.817765, 9.42807),
+            url: 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+            subdomains: 'abcd',
+            maxZoom: 19,
+            icon: icon({
+                iconUrl: require("@/assets/icons/station-off-white.png"),
+                iconSize: [32, 37],
+                iconAnchor: [16, 30]
+            }),
+            sendingicon: icon({
+                iconUrl: require("@/assets/icons/sending-green120.gif"),
+                iconSize: [32, 37],
+                iconAnchor: [16, 30]
+            }),
+            receivingicon: icon({
+                iconUrl: require("@/assets/icons/green-station.png"),
+                iconSize: [32, 37],
+                iconAnchor: [16, 30]
+            }),
+            staticAnchor: [0, 0],
+            customText: "Foobar",
+            iconSize: 64
+        }),
+        methods: {
+            getAntennaForReceiver(repeater, receiverKey) {
+                if (repeater.antenna === undefined) {
+                    return {"location": {"lat": 0, "lon": 0}}
+                }
+                // eslint-disable-next-line no-prototype-builtins
+                if (repeater.antenna.hasOwnProperty(receiverKey)) {
+                    return repeater.antenna[receiverKey];
+                }
+                return repeater.antenna["HilversumH"];
+            },
+            getProxyURL(uri) {
+                uri = uri.replace("http://85.222.223.251:5310/", "https://signal.3ef.de/dm0max-5310/");
+                uri = uri.replace("http://2bs7fpf8ahdewu8a.myfritz.net:1541/", "https://signal.3ef.de/dm0max-1541/");
+                uri = uri.replace("http://2bs7fpf8ahdewu8a.myfritz.net:1538/", "https://signal.3ef.de/dm0max-1538/");
+                uri = uri.replace("http://2bs7fpf8ahdewu8a.myfritz.net:1535/", "https://signal.3ef.de/dm0max-1535/");
+                uri = uri.replace("http://2bs7fpf8ahdewu8a.myfritz.net:1542/", "https://signal.3ef.de/dm0max-1542/");
+                uri = uri.replace("http://db0hhh.de:1535/", "https://signal.3ef.de/db0hhh-1535/");
+                return uri;
+            },
+            setHighZIndex(event) {
+                console.log("HIGH INDEX", event)
+                event.target.setZIndexOffset(1000000);
+            }
+        },
+        computed: {
+            dynamicSize() {
+                return [this.iconSize, this.iconSize * 1.15];
+            },
+            dynamicAnchor() {
+                return [this.iconSize / 2, this.iconSize * 1.15];
+            }
+        },
+        beforeDestroy() {
+            this.repeaters.forEach(repeater => {
+                if (repeater.sse) {
+                    repeater.sse.close();
+                }
+            })
+        }
+    }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,32 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import vuetify from './plugins/vuetify';
+import 'leaflet/dist/leaflet.css';
+import {LMap, LTileLayer, LMarker, LCircleMarker, LTooltip} from 'vue2-leaflet';
+import LControlFullscreen from 'vue2-leaflet-fullscreen';
+import { Icon } from 'leaflet';
+import VueSSE from 'vue-sse';
 
 Vue.config.productionTip = false
+Vue.use(VueSSE);
+
+Vue.component('l-map', LMap);
+Vue.component('l-tile-layer', LTileLayer);
+Vue.component('l-marker', LMarker);
+Vue.component('l-circle-marker', LCircleMarker);
+Vue.component('l-control-fullscreen', LControlFullscreen);
+Vue.component('l-tooltip', LTooltip);
+
+delete Icon.Default.prototype._getIconUrl;
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
 
 new Vue({
-  render: h => h(App),
+  router,
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
